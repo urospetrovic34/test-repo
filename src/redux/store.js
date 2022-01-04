c82c7fc38ee13622d7835c9206fb7eb07dbf162e
UROS <nonamen212@gmail.com>
2022-01-04 17:34:51 +0100

    Deploy take #4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import {createStore,compose, applyMiddleware} from 'redux';
+import {createStore,/*,compose,*/ applyMiddleware} from 'redux';
 import rootReducer from './reducers'
 import {persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
