d9e939818b4a5d4875bc8ecdcf6ff36f7aa74930
UROS <nonamen212@gmail.com>
2022-02-02 10:38:18 +0100

    Updated something 2

--- src/config/axiosPublicConfig.js
+++ src/config/axiosPublicConfig.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://strapi-internship-hr-app.onrender.com/api"
+  baseURL: "https://strapi-internship-hr-app.onrender.com/api",
+  withCredentials: false,
+  headers: {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE',
+  }
 });
 
 instance.defaults.headers.post['Content-Type'] = 'application/json'
